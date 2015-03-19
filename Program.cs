327d7e3358125f4d8502216cc6506f68fad14234
Andy Joiner <andy.joiner@tekkies.co.uk>
2015-03-19 21:18:35 +0000

    Added code.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Web;
+
+namespace UrlEncode.Console
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var stringBuilder = new StringBuilder();
+            var next = System.Console.Read();
+            while (next != -1)
+            {
+                var ch = Convert.ToChar(next);
+                stringBuilder.Append(ch);
+                next = System.Console.Read();
+            }
+            var output = HttpUtility.UrlEncode(stringBuilder.ToString());
+            System.Console.Write(output);
+        }
+    }
+}
